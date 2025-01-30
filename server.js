0caa0f6e3d7b7c37fae8bb1a6b94ba2d5e6cddc6
윤제영 <goods0103@naver.com>
2025-01-30 16:10:10 +0900

    mongo connect 1

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express()
+const { MongoClient } = require('mongodb')
 
-app.listen(8000, () => {
+let db
+const url = 'mongodb+srv://jyjy301:lego4554**@learnmongo.uxkdd.mongodb.net/?retryWrites=true&w=majority&appName=LearnMongo'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8000, () => {});
 
-});
+}).catch((err)=>{
+  console.log(err)
+})
+
+app.use(express.static(__dirname + '/public'));  
+
 
 app.get('/', (request, response) => {
     response.send('ㅎㅇ')
 })
 
-app.get('/shop', (request, response) => {
-    response.send('shopping')
+app.get('/list', async (req, res) => {
+    let result = await db.collection('post').find().toArray();
+    console.log(result);
+    res.send(result)
 })
 
 app.get('/about', (req, res) => {
