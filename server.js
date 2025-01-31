352f4dc0a49e1d444f41a28e7b6cdb5a87ddd8e6
윤제영 <p.jyjy301@kalmate.net>
2025-01-31 17:04:57 +0900

    commit 2

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const { MongoClient } = require('mongodb')
 
 let db
 const url = 'mongodb+srv://jyjy301:lego4554**@learnmongo.uxkdd.mongodb.net/?retryWrites=true&w=majority&appName=LearnMongo'
-new MongoClient(url).connect().then((client)=>{
+new MongoClient(url).connect().then((client) => {
   console.log('DB연결성공')
   db = client.db('forum')
-  app.listen(8000, () => {});
+  app.listen(8000, () => { });
 
-}).catch((err)=>{
+}).catch((err) => {
   console.log(err)
 })
 
@@ -17,27 +17,36 @@ new MongoClient(url).connect().then((client)=>{
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 app.use(express.json())
-app.use(express.urlencoded({extended:true})) 
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (request, response) => {
-    response.send('ㅎㅇ')
+  response.send('ㅎㅇ')
 })
 
 app.get('/write', (request, response) => {
   response.render('write.ejs')
 })
 
-app.post('/add', (req, res) => {
-  db.collection('post').insertOne({title: req.body.title, content: req.body.content})
-  res.render('write.ejs')
+app.post('/add', async (req, res) => {
+  if (req.body.title == '') {
+    console.log('제목 미작성')
+  }
+  else {
+    try {
+      await db.collection('post').insertOne({ title: req.body.title, content: req.body.content })
+      res.render('write.ejs')
+    } catch (error) {
+      res.send('에러남')
+    }
+  }
 })
 
 app.get('/list', async (req, res) => {
-    let result = await db.collection('post').find().toArray();
-    res.render('list.ejs', {data : result});
+  let result = await db.collection('post').find().toArray();
+  res.render('list.ejs', { data: result });
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/introduce.html')
+  res.sendFile(__dirname + '/introduce.html')
 })
